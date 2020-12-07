3670021e5057f748af4a658a40ec1ed1ebec737c
Renit Anthony <renit12345@gmail.com>
2020-12-07 22:55:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,21 +22,21 @@ function staticLoadPlaces() {
 
 var models = [
     {
-        url: './assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
+        url: './assets/magnemite/pink.gltf',
+        scale: '0.02 0.02 0.02',
         info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 180 0',
     },
     {
-        url: './assets/articuno/scene.gltf',
-        scale: '0.2 0.2 0.2',
-        rotation: '0 180 0',
+        url: './assets/articuno/green.gltf',
+        scale: '0.01 0.02 0.02',
+        rotation: '0 0 0',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
     {
-        url: './assets/dragonite/scene.gltf',
-        scale: '0.08 0.08 0.08',
-        rotation: '0 180 0',
+        url: './assets/dragonite/jasmine.gltf',
+        scale: '0.01 0.01 0.01',
+        rotation: '0 0 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
     },
 ];
